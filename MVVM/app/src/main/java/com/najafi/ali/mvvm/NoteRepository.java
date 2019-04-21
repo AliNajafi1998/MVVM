621cea9ddd7157bf2c1b6f81b25f63f4dd1e6a0e
alinajafi <aaaalinajafi1377@gmail.com>
2019-04-21 20:14:02 +0430

    Migrated to AndroidX

--- MVVM/app/src/main/java/com/najafi/ali/mvvm/NoteRepository.java
+++ MVVM/app/src/main/java/com/najafi/ali/mvvm/NoteRepository.java
@@ -1,7 +1,7 @@
 package com.najafi.ali.mvvm;
 
 import android.app.Application;
-import android.arch.lifecycle.LiveData;
+import androidx.lifecycle.LiveData;
 import android.os.AsyncTask;
 
 import java.util.List;
